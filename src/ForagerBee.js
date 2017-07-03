824cf0e42a1b6c13a00fd985789e1767d5025b2d
Hack Reactor Student <student@hackreactor.com>
2017-07-03 11:14:13 -0500

    Passed all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
 var ForagerBee = function() {
+  Grub.call(this);
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+  
+  
 };
 
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(value) {
+  this.treasureChest.push(value);
+}
